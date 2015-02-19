9b2af0d9bd015b23605636803c046972c8966f18
Samuel Bourque <sam@crypto-law.com>
2015-02-19 18:26:13 +0000

    Reduced contract list to uniques

--- NScrypt/app/controllers/contracts_controller.rb
+++ NScrypt/app/controllers/contracts_controller.rb
@@ -8,7 +8,7 @@ class ContractsController < ApplicationController
     # Also get where the user was a proposed party
     parties = Party.where("user_id = ? AND (state = 'Proposed' OR state = 'Signed')", session[:user_id])
     #parties = Party.where("user_id = ?", session[:user_id])
-    parties.each{ |party| @contracts << party.code.contract }
+    parties.each{ |party| @contracts << party.code.contract unless @contracts.include?(party.code.contract) }
   end
 
   # GET /contracts/1
