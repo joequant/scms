ed440200884f4fd74744652beecad86e18fed65e
Samuel Bourque <sam@crypto-law.com>
2015-02-19 06:18:45 +0000

    Revert "Added hash field to code version"
    This reverts commit f9f4059f069df34ce202bbe2f2173bbf39b83930.

--- NScrypt/db/migrate/20150218100425_add_signed_code_to_contract.rb
+++
@@ -1,5 +0,0 @@
-class AddSignedCodeToContract < ActiveRecord::Migration
-  def change
-    add_column :contracts, :signed_code, :has_one
-  end
-end
--- NScrypt/db/migrate/20150219055853_add_hash_to_code.rb
+++
@@ -1,5 +0,0 @@
-class AddHashToCode < ActiveRecord::Migration
-  def change
-    add_column :codes, :hash, :string
-  end
-end
