d1a5e1acba76dae74cdede269688cecabea59c72
Han Kessels <han.kessels@ntt.eu>
2015-02-23 22:50:56 +0000

    Add Devise authentication

--- NScrypt/config/environments/development.rb
+++ NScrypt/config/environments/development.rb
@@ -50,4 +50,6 @@ Rails.application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
+    config.action_mailer.default_url_options = { host: 'www.crypto-law.com' }
+
 end
