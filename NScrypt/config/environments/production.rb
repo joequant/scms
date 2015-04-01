27352b1a69acca88a222857c3c78d70e26762b84
Han Kessels <han.kessels@ntt.eu>
2015-04-01 13:35:43 +0900

    mailer config for production

--- NScrypt/config/environments/production.rb
+++ NScrypt/config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "mail.crypto-law.com",
+    :port                 => 26,
+    :user_name            => 'admin@crypto-law.com',
+    :password             => 'GqN=Cbd{~]&P',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { host: 'http://106.186.121.212'}
+
 end
