903279f8cc4e9a16a839a6409ea9ccd363a08035
Samuel Bourque <sam@crypto-law.com>
2015-04-21 17:23:44 +0000

    Changed URL for email links

--- NScrypt/config/environments/production.rb
+++ NScrypt/config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { host: 'http://106.186.121.212'}
+  config.action_mailer.default_url_options = { host: 'http://nscrypt.io'}
 
 end
