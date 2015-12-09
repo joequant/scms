b3b32296b141e677da941a08a3595f576fc2e58d
Joseph C Wang <joequant@gmail.com>
2015-12-09 08:46:36 +0800

    add install packages

---
+++ docker/install-packages-mageia.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+urpmi --no-recommends --no-md5sum --excludedocs --auto \
+      apache ruby-bundler ruby-devel gcc ruby-io-console \
+      sqlite3-devel zlib-devel libxml2-devel make nodejs \
+      apache apache-mod_proxy apache-mod_ssl
