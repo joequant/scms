47c89faa0978aacea26c8fe60292af9a354a4c84
Joseph C Wang <joequant@gmail.com>
2015-12-13 19:28:57 +0800

    add items

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $SCRIPT_DIR/../mayan-edms
+virtualenv venv
+source venv/bin/activate
+mayan-edms.py runserver &
+popd
 pushd $SCRIPT_DIR/NScrypt
 ./bin/rails server --binding=0.0.0.0
 popd
