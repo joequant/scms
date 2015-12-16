9e793e25efd50534a5bf6b0198214839a9408b07
Joseph C Wang <joequant@gmail.com>
2015-12-16 15:54:37 +0800

    use gitlist

--- docker/install-packages-mageia.sh
+++ docker/install-packages-mageia.sh
@@ -3,7 +3,7 @@ urpmi --no-recommends --no-md5sum --excludedocs --auto \
       apache ruby-bundler ruby-devel gcc ruby-io-console \
       sqlite3-devel zlib-devel libxml2-devel make nodejs \
       apache apache-mod_proxy apache-mod_ssl \
-      apache-mod_proxy_html \
+      apache-mod_proxy_html apache-mod_php \
       python-pip python-pillow python-yaml \
       python-dateutil python-pytz gnupg \
       python-virtualenv jpeg-devel python-devel \
