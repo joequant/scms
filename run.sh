f8a06732c84e951f8fcf473bc2ed5c62f02539e2
Joseph C Wang <joequant@gmail.com>
2016-01-16 15:03:58 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../mayan-edms
 source env-mayan/bin/activate
 mayan-edms.py runserver &
-source env-mayan/bin/deactivate
+deactivate
 popd
+pushd $SCRIPT_DIR/../allura
 source env-allura/bin/activate
 paster setup-app Allura/development.ini
 gunicorn --reload --paste Allura/development.ini --daemon
-source env-allura/bin/deactivate
+deactivate
 popd
 pushd $SCRIPT_DIR/NScrypt
 ./bin/rails server --binding=0.0.0.0
