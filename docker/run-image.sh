5e70fc60496b20521f36a83ff1fc849738b36340
Joseph C Wang <joequant@gmail.com>
2016-02-07 20:15:42 +0800

    change run scripts

--- docker/run-image.sh
+++ docker/run-image.sh
@@ -4,5 +4,5 @@ su mongod -s "/bin/bash" -c "/usr/bin/mongod --quiet -f /etc/mongod.conf" &
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm /home/user/git/scms/NScrypt/tmp/pids/server.pid
 service solr start
-su user -c $SCRIPT_DIR/../run.sh
+su user -c $SCRIPT_DIR/run-user.sh
 
