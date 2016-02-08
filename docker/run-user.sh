2e471dbbbbe59a2a9b8b67de22f59cc4c64f778f
Joseph C Wang <joequant@gmail.com>
2016-02-09 00:58:28 +0800

    fix run user

--- docker/run-user.sh
+++ docker/run-user.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $SCRIPT_DIR/../mayan-edms
+pushd $SCRIPT_DIR/../../mayan-edms
 source env-mayan/bin/activate
 mayan-edms.py runserver &
 deactivate
 popd
-pushd $SCRIPT_DIR/../allura
+pushd $SCRIPT_DIR/../../allura
 source env-allura/bin/activate
 paster setup-app Allura/development.ini
 gunicorn --reload --paste Allura/development.ini --daemon
