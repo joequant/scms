a26d62ebb6d0a9177a5e7fd0446b8a053eaaa798
Joseph C Wang <joequant@gmail.com>
2016-02-09 11:26:49 +0800

    set up docker script so that it works with root

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -5,10 +5,12 @@ if [ "$1" != "" ] ; then
    IMAGE=$1
 fi
 
-mkdir -p ~/volumes/$IMAGE
-pushd ~/volumes/$IMAGE
+IMAGEDIR=$HOME/volumes/$IMAGE
 
-if [ ! -e ~/volumes/$IMAGE/home ] ; then
+mkdir -p $IMAGEDIR
+pushd $IMAGEDIR
+
+if [ ! -e $IMAGEDIR/home ] ; then
 # This will fail on docker < 1.8
 id=$($SUDO docker create $IMAGE)
 $SUDO docker cp $id:/home - | tar xf -
@@ -16,28 +18,28 @@ $SUDO docker rm -v $id
 fi
 
 
-if [ ! -e ~/volumes/$IMAGE/var/log ] ; then
+if [ ! -e $IMAGEDIR/var/log ] ; then
 mkdir -p var/log
 chmod a+rwx var/log
 $SUDO docker run \
--v ~/volumes/$IMAGE/var:/mnt \
+-v $IMAGEDIR/var:/mnt \
 $IMAGE \
 cp -a -P -R /var/log /mnt
 fi
 
-if [ ! -e ~/volumes/$IMAGE/etc ] ; then
+if [ ! -e $IMAGEDIR/etc ] ; then
 mkdir -p etc
 chmod a+rwx etc
 $SUDO docker run \
--v ~/volumes/$IMAGE:/mnt \
+-v $IMAGEDIR:/mnt \
 $IMAGE \
 cp -a -P -R /etc /mnt
 fi
 
 $SUDO docker run \
 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
--v ~/volumes/$IMAGE/home:/home \
--v ~/volumes/$IMAGE/etc:/etc \
+-v $IMAGEDIR/home:/home \
+-v $IMAGEDIR/etc:/etc \
 -p 80:80 -p 8000:8000 -p 8080:8080 -p 3000:3000 $IMAGE &
 echo "Docker started"
 
